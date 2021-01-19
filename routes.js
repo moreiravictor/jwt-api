23142576d1b0b3086cc565f9a72ab3c817451b77
Victor Moreira <victormoreira176@hotmail.com>
2021-01-19 16:09:08 -0300

    :hammer: renaming route functions

--- routes.js
+++ routes.js
@@ -6,11 +6,11 @@ const loginController = require('./adapter/http/controller/LoginController');
 const productController = require('./adapter/http/controller/ProductController');
 
 routes.post('/login', loginController.login);
-routes.post('/product', authService.verify, productController.postProduct);
+routes.post('/product', authService.verify, productController.post);
 
-routes.get('/product/:product_id', authService.verify, productController.getProduct);
-routes.get('/product', authService.verify, productController.getAllProducts);
+routes.get('/product/:product_id', authService.verify, productController.get);
+routes.get('/product', authService.verify, productController.getAll);
 
-routes.patch('/product/:product_id', authService.verify, productController.patchProduct);
+routes.patch('/product/:product_id', authService.verify, productController.patch);
 
 module.exports = routes;
